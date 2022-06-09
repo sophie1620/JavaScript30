03b321f31c7331dc50082fa5c5218f9a0acf98e2
sophie <sophie1620@gmail.com>
2022-06-09 11:40:01 -0400

    attempted lesson 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,14 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+    .then(localMedialStream => {
+        console.log(localMedialStream, video);
+        video.src = window.URL.createObjectURL(localMedialStream);
+        video.play();
+    });
+}
+
+getVideo();
